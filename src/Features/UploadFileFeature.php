8427f25ef9fb109b514d13a41f093a3dd456e3a6
Vital Ozierski <v.ozierski@ozitag.com>
2020-12-17 20:10:40 +0300

    basic implementation for "upload by URL"

--- src/Features/UploadFileFeature.php
+++ src/Features/UploadFileFeature.php
@@ -2,16 +2,56 @@
 
 namespace OZiTAG\Tager\Backend\Files\Features;
 
+use Illuminate\Http\Request;
 use Ozerich\FileStorage\Storage;
 use Ozerich\FileStorage\Models\File;
 use OZiTAG\Tager\Backend\Core\Features\Feature;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UploadFileFeature extends Feature
 {
+    protected bool $supportUrl;
+
+    protected bool $supportFile;
+
+    public function __construct($supportFile = true, $supportUrl = false)
+    {
+        $this->supportFile = $supportFile;
+
+        $this->supportUrl = $supportUrl;
+
+        if (!$this->supportUrl || !$this->supportFile) {
+            throw new \Exception('UploadFileFeature must have minimum one enabled mode');
+        }
+    }
+
     public function handle(Storage $storage)
     {
-        /** @var File $file */
-        $file = $storage->createFromRequest();
+        $file = null;
+
+        if ($this->supportFile) {
+            /** @var Request $request */
+            $request = app()->request;
+
+            $requestFile = $request->file('file');
+            if ($requestFile) {
+                $file = $storage->createFromRequest();
+            }
+        }
+
+        if (!$file && $this->supportUrl) {
+            $url = $request->post('url');
+
+            if (empty($url)) {
+                abort(400, 'Empty URL');
+            }
+
+            if (!filter_var($url, FILTER_VALIDATE_URL)) {
+                abort(400, 'Invalid URL');
+            }
+
+            $file = $storage->createFromUrl($url);
+        }
 
         if (!$file) {
             abort(400, $storage->getUploadError());
--- src/TagerBackendFilesController.php
+++ src/TagerBackendFilesController.php
@@ -2,13 +2,32 @@
 
 namespace OZiTAG\Tager\Backend\Files;
 
-use OZiTAG\Tager\Backend\Core\Controller;
+use OZiTAG\Tager\Backend\Core\Controllers\Controller;
 use OZiTAG\Tager\Backend\Files\Features\UploadFileFeature;
 
 class TagerBackendFilesController extends Controller
 {
     public function upload()
     {
-        return $this->serve(UploadFileFeature::class);
+        return $this->serve(UploadFileFeature::class, [
+            'supportUrl' => true,
+            'supportFile' => true
+        ]);
+    }
+
+    public function uploadByUrl()
+    {
+        return $this->serve(UploadFileFeature::class, [
+            'supportUrl' => true,
+            'supportFile' => false
+        ]);
+    }
+
+    public function uploadByFile()
+    {
+        return $this->serve(UploadFileFeature::class, [
+            'supportUrl' => false,
+            'supportFile' => true
+        ]);
     }
 }
