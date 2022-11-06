cd31a6dd5689faffacb8b6bc3e285d343b5d092a
Vital Ozierski <vital@ozerich.com>
2022-11-06 04:28:44 +0300

    1.3.2 - add base64 format to Upload feature

--- src/Features/UploadFileFeature.php
+++ src/Features/UploadFileFeature.php
@@ -10,23 +10,15 @@ use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class UploadFileFeature extends Feature
 {
-    protected bool $supportUrl;
-
-    protected bool $supportFile;
-
-    protected ?string $scenario;
-
-    public function __construct(bool $supportFile = true, bool $supportUrl = false, ?string $scenario = null)
+    public function __construct(protected bool    $supportFile = true,
+                                protected bool    $supportUrl = false,
+                                protected bool    $supportBase64 = false,
+                                protected ?string $scenario = null
+    )
     {
-        $this->supportFile = $supportFile;
-
-        $this->supportUrl = $supportUrl;
-
-        if (!$this->supportUrl && !$this->supportFile) {
-            throw new \Exception('UploadFileFeature must have minimum one enabled mode');
+        if (!$this->supportUrl && !$this->supportFile && !$this->supportBase64) {
+            throw new \Exception('UploadFileFeature must have minimum one enabled mode (file, url, base64)');
         }
-
-        $this->scenario = $scenario;
     }
 
     protected function throwError($message)
@@ -49,12 +41,35 @@ class UploadFileFeature extends Feature
         $usedFile = false;
 
         if ($this->supportFile) {
-            /** @var Request $request */
-            $request = app()->request;
-
             $requestFile = $request->file('file');
             if ($requestFile) {
                 $file = $storage->createFromRequest($scenario);
+                $usedFile = true;
+            }
+        }
+
+        if (!$usedFile && $this->supportBase64) {
+            $base64 = $request->post('base64');
+            $filename = $request->post('filename');
+
+            if (str_starts_with($base64, 'data:image/png;base64,')) {
+                $base64 = substr($base64, strlen('data:image/png;base64,'));
+                if (!$filename) {
+                    $filename = 'file.png';
+                }
+            } else if (str_starts_with($base64, 'data:image/jpeg;base64,')) {
+                $base64 = substr($base64, strlen('data:image/jpeg;base64,'));
+                if (!$filename) {
+                    $filename = 'file.jpg';
+                }
+            }
+
+            if (!empty($base64)) {
+                if (base64_encode(base64_decode($base64, true)) !== $base64) {
+                    $this->throwError('Invalid base64');
+                }
+
+                $file = $storage->createFromBase64($base64, $filename, $scenario);
                 $usedFile = true;
             }
         }
@@ -68,18 +83,11 @@ class UploadFileFeature extends Feature
                 $this->throwError('Invalid URL');
             } else {
                 $file = $storage->createFromUrl($url, $scenario);
-                if(!$file){
-                    $this->throwError('Failure download file - '.$storage->getUploadError());
-                }
             }
         }
 
         if (!$file) {
-            if ($this->supportFile && !$request->file('file')) {
-                $this->throwError('File is empty');
-            } else {
-                $this->throwError($storage->getUploadError());
-            }
+            $this->throwError('Failure create file - ' . $storage->getUploadError());
         }
 
         return $file->getShortJson();
